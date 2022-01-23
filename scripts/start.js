f8caaea05536149611dbb3eb83a001e9c72bafcc
Илья <70086932+Gorvarydar@users.noreply.github.com>
2022-01-23 18:11:44 +0300

    hapi

--- scripts/start.js
+++ scripts/start.js
@@ -6,17 +6,17 @@ const port = process.env.PORT || 3002;
 const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png|ico)(\?v=\d+\.\d+\.\d+)?$/;
 
 const PATH = {
-    '/': 'index.html',
-    '/': 'main.html'
+    '/': 'index.html'
+}
+const PATHS = {
+    '/':'main.html'
 }
 
 const init = async () => {
     const server = Hapi.server({
         port,
     });
-
     await server.register(require('@hapi/inert'));
-
     server.route({
         method: 'GET',
         path: '/{path*}',
@@ -25,9 +25,21 @@ const init = async () => {
                 return h.file(path.join(process.cwd(), 'public', request.path));
             }
                 console.log(request.path)
-            return h.file(path.join(process.cwd(), 'public', PATH[request.path]));
+                return h.file(path.join(process.cwd(), 'public', PATH[request.path]));
         }
     })
+    server.route({
+        method: 'GET',
+        path: '/main.html',
+        handler: (request, h) => {
+            if (FILES.test(request.path)) {
+                return h.file(path.join(process.cwd(), 'public', request.path));
+            }
+                console.log(request.path)
+                return h.file(path.join(process.cwd(), 'public', PATHS[request.path]));
+        }
+    })
+
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
